eccba487589fe3f6294e19779b76379d76983302
harufukuda <harufukuda14@gmail.com>
2023-07-18 20:48:08 +0900

    add: Add index and store method

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        Task::create($request->all());
+        return response()->json([
+            'message' => 'Created successfully',
+        ]);
+    }
 }
